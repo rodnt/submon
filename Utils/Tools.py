6b30081f66f8226401947bd4ee303fff7255f390
rodnt <101719434+rodnt@users.noreply.github.com>
2023-08-26 09:24:22 -0300

    add nuclei paths..

--- Utils/Tools.py
+++ Utils/Tools.py
@@ -5,10 +5,12 @@ from datetime import datetime
 
 
 class Tools:
-    def __init__(self):
+    def __init__(self, nuclei_iserver="", nuclei_severity="high,critical", nuclei_templates_path=""):
         self.commands = ["nuclei"]
         self.iserver = ""
-        self.severity = ""
+        self.eid = "" # TODO
+        self.severity = nuclei_severity
+        self.nuclei_templates_path = nuclei_templates_path
 
     def check_tools(self):
         for command in self.commands:
@@ -22,7 +24,27 @@ class Tools:
                 return
         else:
             current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            cmd = ['nuclei', '-l', "new_subdomains.txt", '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration', '-o', 'nuclei_results.txt', '-s','high,critical']
+
+            cmd = ""
+
+            if self.nuclei_templates_path != "":
+                 if self.iserver != "":
+                    cmd = ['nuclei', '-l', file_new_subdomains, '-t', self.nuclei_templates_path, '-o', 'nuclei_results.txt', '-s', 'high,critical', '-iserver', self.iserver]
+                 if self.iserver != "" and self.severity != "high,critical":
+                    cmd = ['nuclei', '-l', file_new_subdomains, '-t', self.nuclei_templates_path, '-o', 'nuclei_results.txt', '-s', self.severity, '-iserver', self.iserver]
+                 if self.iserver == "":
+                    cmd = ['nuclei', '-l', file_new_subdomains, '-t', self.nuclei_templates_path, '-o', 'nuclei_results.txt', '-s', 'high,critical']
+                 
+
+            if self.iserver != "":
+                cmd = ['nuclei', '-l', file_new_subdomains, '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration', '-t', '/root/nuclei-templates/http/takeovers', '-o', 'nuclei_results.txt', '-s', 'high,critical', '-iserver', self.iserver]
+            
+            if self.iserver != "" and self.severity != "high,critical":
+                cmd = ['nuclei', '-l', file_new_subdomains, '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration', '-t', '/root/nuclei-templates/http/takeovers', '-o', 'nuclei_results.txt', '-s', self.severity, '-iserver', self.iserver]
+
+            if self.iserver == "":
+                cmd = ['nuclei', '-l', "new_subdomains.txt", '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration','-t','/root/nuclei-templates/http/takeovers' '-o', 'nuclei_results.txt', '-s','high,critical']
+
             try:
                 result = subprocess.run(cmd, capture_output=True, text=True)
                 if os.path.isfile('nuclei_results.txt'):
@@ -34,7 +56,25 @@ class Tools:
     
     def run_single_nuclei(self, domain):
         current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        cmd = ['nuclei', '-u', domain, '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration', '-o', 'nuclei_results.txt', '-s', 'high,critical']
+        cmd = ""
+
+        if self.nuclei_templates_path != "":
+                 if self.iserver != "":
+                    cmd = ['nuclei', '-u', domain, '-t', self.nuclei_templates_path, '-o', 'nuclei_results.txt', '-s', 'high,critical', '-iserver', self.iserver]
+                 if self.iserver != "" and self.severity != "high,critical":
+                    cmd = ['nuclei', '-u', domain, '-t', self.nuclei_templates_path, '-o', 'nuclei_results.txt', '-s', self.severity, '-iserver', self.iserver]
+                 if self.iserver == "":
+                    cmd = ['nuclei', '-u', domain, '-t', self.nuclei_templates_path, '-o', 'nuclei_results.txt', '-s', 'high,critical']
+
+        if self.iserver != "":
+            cmd = ['nuclei', '-u', domain, '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration', '-t', '/root/nuclei-templates/http/takeovers', '-o', 'nuclei_results.txt', '-s', 'high,critical', '-iserver', self.iserver]
+        
+        if self.iserver != "" and self.severity != "high,critical":
+            cmd = ['nuclei', '-u', domain, '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration', '-t', '/root/nuclei-templates/http/takeovers', '-o', 'nuclei_results.txt', '-s', self.severity, '-iserver', self.iserver]
+            
+        if self.iserver == "":
+            cmd = ['nuclei', '-u', domain, '-t', '/root/nuclei-templates/http/cves', '-t', '/root/nuclei-templates/http/misconfiguration','-t','/root/nuclei-templates/http/takeovers' '-o', 'nuclei_results.txt', '-s','high,critical']
+
         try:
             result = subprocess.run(cmd, capture_output=True, text=True)
             if os.path.isfile('nuclei_results.txt'):
@@ -43,5 +83,3 @@ class Tools:
         except Exception as e:
                 print(f"Error encountered: {e}")
                 return None
-        
-    # TODO subdomain takeover :P
